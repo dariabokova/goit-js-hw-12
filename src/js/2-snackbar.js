c75137ae6bdfba33288dc4fe11b53da19bd33285
Daria <dariaa.bokova@gmail.com>
2024-10-06 01:57:17 +0300

    finished

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+
+  const delay = Number(event.target.elements.delay.value);
+
+  const state = event.target.elements.state.value;
+
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
