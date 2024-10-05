dd3f61c56107f6e8ecf71cc53c078037530dab2f
Daria <dariaa.bokova@gmail.com>
2024-10-05 23:53:37 +0300

    edited vite

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,3 @@
-// import { defineConfig } from 'vite';
-// import { glob } from 'glob';
-// import injectHTML from 'vite-plugin-html-inject';
-// import FullReload from 'vite-plugin-full-reload';
-// import SortCss from 'postcss-sort-media-queries';
-
-// export default defineConfig(({ command }) => {
-//   return {
-//     define: {
-//       [command === 'serve' ? 'global' : '_global']: {},
-//     },
-//     root: 'src',
-//     build: {
-//       sourcemap: true,
-//       rollupOptions: {
-//         input: glob.sync('./src/*.html'),
-//         output: {
-//           manualChunks(id) {
-//             if (id.includes('node_modules')) {
-//               return 'vendor';
-//             }
-//           },
-//           entryFileNames: chunkInfo => {
-//             if (chunkInfo.name === 'commonHelpers') {
-//               return 'commonHelpers.js';
-//             }
-//             return '[name].js';
-//           },
-//           assetFileNames: assetInfo => {
-//             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-//               return '[name].[ext]';
-//             }
-//             return 'assets/[name]-[hash][extname]';
-//           },
-//         },
-//       },
-//       outDir: '../dist',
-//       emptyOutDir: true,
-//     },
-//     optimizeDeps: {
-//       include: ['simplelightbox'] // Додано optimizeDeps для включення SimpleLightbox
-//     },
-//     plugins: [
-//       injectHTML(),
-//       FullReload(['./src/**/**.html']),
-//       SortCss({
-//         sort: 'mobile-first',
-//       }),
-//     ],
-//   };
-// });
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
@@ -57,32 +6,36 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/goit-js-hw-09/', // Додаємо base для коректного відображення на GitHub Pages
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
     build: {
       sourcemap: true,
-      outDir: '../dist', // Вказуємо вихідну папку dist у кореневій директорії
-      emptyOutDir: true,
       rollupOptions: {
-        input: {
-          main: 'src/index.html', // Вказуємо index.html у папці src
-        },
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: '[name].js',
-          assetFileNames: 'assets/[name]-[hash][extname]',
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
-    },
-    optimizeDeps: {
-      include: ['simplelightbox'],
+      outDir: '../dist',
+      emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
@@ -93,4 +46,49 @@ export default defineConfig(({ command }) => {
     ],
   };
 });
+
+// import { defineConfig } from 'vite';
+// import { glob } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import SortCss from 'postcss-sort-media-queries';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     base: '/goit-js-hw-09/', // Додаємо base для коректного відображення на GitHub Pages
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       outDir: '../dist', // Вказуємо вихідну папку dist у кореневій директорії
+//       emptyOutDir: true,
+//       rollupOptions: {
+//         input: {
+//           main: './src/index.html', // Вказуємо index.html у папці src
+//         },
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: '[name].js',
+//           assetFileNames: 'assets/[name]-[hash][extname]',
+//         },
+//       },
+//     },
+//     optimizeDeps: {
+//       include: ['simplelightbox'],
+//     },
+//     plugins: [
+//       injectHTML(),
+//       FullReload(['./src/**/**.html']),
+//       SortCss({
+//         sort: 'mobile-first',
+//       }),
+//     ],
+//   };
+// });
 
